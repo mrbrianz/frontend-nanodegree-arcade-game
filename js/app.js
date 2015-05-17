cdcc5fd449c74f957614e5a095550e0f5787a17c
shesellsseashells@me.com <shesellsseashells@me.com>
2015-05-17 18:07:46 -0500

    Add enemy speed, add reset for enemy once reaches end of the canvas (x axis)

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@
 // ===== ENEMY =====
 
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
+    // how fast?
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -20,12 +22,19 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     /// takes this.x coordinate plus itself plus this speed times the dt param
     this.x += this.speed*dt;
-}
+
+    // once passes too far to the right, reset to off the left of the screen at some random point
+    // so, if x value (right/left axis) is greater than the canvas width, put it back to the leftside
+    if (this.x) > ctx.canvas.clientWidth) {
+        this.x = -101; // set off to left of canvas
+        this.speed = getRandomInt(20,100); // randomize new speed, but using a range
+    }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 
 // ===== PLAYER =====
@@ -117,7 +126,12 @@ Player.prototype.handleInput = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
