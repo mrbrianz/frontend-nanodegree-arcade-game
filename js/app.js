3db8341477e1c8a4772194f00ee9f8513548f664
shesellsseashells@me.com <shesellsseashells@me.com>
2015-05-17 20:24:44 -0500

    Add speed1 and speed2 global variables, Add more enemies and incorporate speed1 and speed2, Play with canvas (exploring options)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-
-
+var speed1 = 50;
+var speed2 = 300;
 
 // ===== ENEMY =====
 
@@ -29,7 +29,7 @@ Enemy.prototype.update = function(dt) {
     // so, if x value (right/left axis) is greater than the canvas width, put it back to the leftside
     if (this.x > ctx.canvas.clientWidth) {
         this.x = -101; // set off to left of canvas
-        this.speed = getRandomInt(50,200); // randomize new speed, but using a range
+        this.speed = getRandomInt(speed1,speed2); // randomize new speed, but using a range
     }
 };
 
@@ -49,6 +49,8 @@ var Player = function (x, y) {
     // image to use
     this.sprite = 'images/char-boy.png';
 
+    this.level = 1;
+    this.lives = 3;
     // Maybe set number of lives here?
     // Maybe set score/menu here?
 
@@ -77,12 +79,22 @@ Player.prototype.handleInput = function(keyCode) { // Uses Parameter 'keycode' f
         if (this.y > 1) {
             this.y -= 83; // why 83?  not sure
             if (this.y < 0) {
-                // draw winner square here
-                // tell user to press 'up'
+                console.log(this.x,this.y);
+                this.bg = 'images/grass-block.png';
+                console.log(this.bg);
+                ctx.fillStyle="black";
+                ctx.fillRect(0,0,707,707);
+                ctx.drawImage(Resources.get(this.bg), this.x, this.y);
             }
         /* but if not, then reset to the beginning
         */
         } else {
+            this.level += 1;
+            console.log(this.level);
+            console.log(this.x,this.y);
+            ctx.fillStyle="white";
+            ctx.fillRect(0,0,707,707);
+
             this.x = 303;
             this.y = 485; // now sure on these numbers
             // alert('You Win');
@@ -90,6 +102,7 @@ Player.prototype.handleInput = function(keyCode) { // Uses Parameter 'keycode' f
             if (this.lives === 0) {
                 alert('Game Over');
             }
+
         }
     }
 
@@ -124,14 +137,6 @@ Player.prototype.handleInput = function(keyCode) { // Uses Parameter 'keycode' f
     }
 };
 
-
-
-// Now instantiate your objects.
-// TODO: create allEnemies array
-// TODO: put player object in player variable
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Returns a random integer between min (included) and max (excluded)
 // Using Math.round() will give you a non-uniform distribution!
@@ -156,9 +161,9 @@ document.addEventListener('keyup', function(e) {
 });
 
 // Create (Instantiate) the enemy bugs using the 'allEnemies' array
-// my thinkings is 2 bugs on top row, 1 bug on the next, 2 on the next, and only 1 on the first row that the player will encounter (and start the first one at a slower speed range)
-var Bug1 = new Enemy(0, 60, getRandomInt(50,200)), Bug1b = new Enemy(303, 60, getRandomInt(50,200)), Bug2 = new Enemy(0, 143, getRandomInt(50,200)), Bug3 = new Enemy(0, 230, getRandomInt(50,200), Bug3b = new Enemy(0, 230, getRandomInt(50,200)), Bug4 = new Enemy(0, 310, getRandomInt(50,100)));
-var allEnemies = [Bug1,Bug1b,Bug2,Bug3,Bug3b,Bug4];
+// my thinkings is 3 bugs on top row, 2 bugs on the next, 2 on the next, and only 1 on the first row that the player will encounter (and start the first one at a slower speed range)
+var Bug1 = new Enemy(0, 60, getRandomInt(speed1,speed2)), Bug1b = new Enemy(404, 60, getRandomInt(speed1,speed2)), Bug1c = new Enemy(202, 60, getRandomInt(speed1,speed2)), Bug2 = new Enemy(0, 143, getRandomInt(speed1,speed2)), Bug2b = new Enemy(505, 143, getRandomInt(speed1,speed2)), Bug3 = new Enemy(0, 230, getRandomInt(speed1,speed2), Bug3b = new Enemy(202, 230, getRandomInt(speed1,speed2)), Bug4 = new Enemy(0, 310, getRandomInt(speed1,150)));
+var allEnemies = [Bug1,Bug1b,Bug1c,Bug2,Bug2b,Bug3,Bug3b,Bug4];
 // Create (Instantiate) the player
 var player = new Player(303,485); // 303 makes sense, but I do not yet grasp the y axis amounts
 
