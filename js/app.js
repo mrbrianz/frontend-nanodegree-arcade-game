5a0fd3f50665426382636266123b01acaa1ca973
shesellsseashells@me.com <shesellsseashells@me.com>
2015-05-22 01:19:32 -0500

    Update sequence of added enemies, add various speed increases as player wins

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // speed variables used to provide a range of speeds
 var speed1 = 50;
-var speed2 = 300;
+var speed2 = 270;
 
 // ===== ENEMY =====
 
@@ -15,7 +15,6 @@ var Enemy = function(x, y, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-
 };
 
 // Update the enemy's position, required method for game
@@ -31,7 +30,13 @@ Enemy.prototype.update = function(dt) {
     // so, if x value (right/left axis) is greater than the canvas width, put it back to the leftside
     if (this.x > ctx.canvas.clientWidth) {
         this.x = -101; // set off to left of canvas
-        this.speed = getRandomInt(speed1,speed2); // randomize new speed, but using a range
+
+        // if level 12 or above, increase the range on the next to the last lane (we want lots of collisions here in the upper levels)
+        if (this.level > 11 && this.y === 143) {
+            this.speed = getRandomInt((speed1+this.level),(speed2+(this.level*3))); // randomize new speed, but using a range
+        } else {
+            this.speed = getRandomInt(speed1,speed2); // randomize new speed, but using a range
+        }
     }
 };
 
@@ -56,9 +61,6 @@ var Player = function (x, y) {
     this.score = 0;
     // Maybe set number of lives here?
     // Maybe set score/menu here?
-
-
-
 };
 
 // Player update method
@@ -82,7 +84,7 @@ Player.prototype.handleInput = function(keyCode) { // Uses Parameter 'keycode' f
             take itself minus 101 when clicking 'up' because each rows is 101
         */
         if (this.y > 1) {
-            this.y -= 83; // why 83?  not sure
+            this.y -= 83; // 83 is defined in 'engine.js'
             if (this.y < 0) {
                 // set large text at top announcing a win
                 ctx.clearRect(0,0,707,200);
@@ -106,9 +108,41 @@ Player.prototype.handleInput = function(keyCode) { // Uses Parameter 'keycode' f
             ctx.clearRect(0,0,707,200);
             ctx.clearRect(200,600,400,200);
 
+            // reset player to starting point
             this.x = 303;
-            this.y = 485; // not sure on these numbers
-            // alert('You Win');
+            this.y = 485;
+
+            // first row of enemies (from top down)
+            if (this.level === 3 || this.level === 9) {
+                // add enemy at level 3 and 9
+                allEnemies.push(new Enemy(0, 60, getRandomInt(speed1,speed2)));
+            }
+            // second row of enemies (from top down)
+            if (this.level === 2 || this.level === 3) {
+                // add enemy at level 2 and 3
+                allEnemies.push(new Enemy(0, 143, getRandomInt(speed1,speed2)));
+            }
+            // third row of enemies (from top down)
+            if (this.level === 5 || this.level === 7) {
+                // add enemy at level 5 and 7
+                allEnemies.push(new Enemy(0, 230, getRandomInt(speed1,speed2)));
+            }
+            // fourth row of enemies (from top down)
+            if (this.level === 10) {
+                // add enemy at level 10
+                allEnemies.push(new Enemy(0, 310, getRandomInt(speed1,speed2)));
+            }
+
+            if (this.level > 7) {
+                // at level 8, add additional speed to both the lower (speed1) and upper (speed2) ranges of speed
+                window.speed1 = window.speed1 + 5;
+                window.speed2 = window.speed2 + 2;
+            }
+
+            // increment 'up' the ranges for variable speed (gap will slowly grow, and range will slowly increment up on each win)
+            //window.speed1 = window.speed1 + 1;
+            window.speed2 = window.speed2 + 5;
+
             // use up certain amt of lives, then game is over
                 ctx.font = "bold 20px serif";
                 ctx.textAlign = "center";
@@ -132,7 +166,7 @@ Player.prototype.handleInput = function(keyCode) { // Uses Parameter 'keycode' f
         /* if y coord (up/down axis) is still less than 607 (6 rows X 101),
             take itself plus 101 when clicking 'down' because each row is 101
         */
-        if (this.y < 450) { // why 450?  not sure
+        if (this.y < 450) {
             if (this.y < 0) {
                 // if less than 0 (if now in top row), disallow going down, because they have already won the level
             } else {
@@ -194,9 +228,9 @@ document.addEventListener('keyup', function(e) {
 });
 
 // Create (Instantiate) the enemy bugs using the 'allEnemies' array
-// my thinkings is 3 bugs on top row, 2 bugs on the next, 2 on the next, and only 1 on the first row that the player will encounter (and start the first one at a slower speed range)
-var Bug1 = new Enemy(0, 60, getRandomInt(speed1,speed2)), Bug1b = new Enemy(404, 60, getRandomInt(speed1,speed2)), Bug1c = new Enemy(202, 60, getRandomInt(speed1,speed2)), Bug2 = new Enemy(0, 143, getRandomInt(speed1,speed2)), Bug2b = new Enemy(505, 143, getRandomInt(speed1,speed2)), Bug3 = new Enemy(0, 230, getRandomInt(speed1,speed2), Bug3b = new Enemy(202, 230, getRandomInt(speed1,speed2)), Bug4 = new Enemy(0, 310, getRandomInt(speed1,150)));
-var allEnemies = [Bug1,Bug1b,Bug1c,Bug2,Bug2b,Bug3,Bug3b,Bug4];
+// my thinkings is 3 bugs on top row, 2 bugs on the next, 3 on the next, and 2 on the first row that the player will encounter (and start the first one at a slower speed range)
+var Bug1 = new Enemy(0, 60, getRandomInt(speed1,speed2)), Bug3 = new Enemy(0, 230, getRandomInt(speed1,speed2), Bug4 = new Enemy(0, 310, getRandomInt(speed1,150)));
+var allEnemies = [Bug1,Bug3,Bug4];
 // Create (Instantiate) the player
 var player = new Player(303,485); // 303 makes sense, but I do not yet grasp the y axis amounts
 
