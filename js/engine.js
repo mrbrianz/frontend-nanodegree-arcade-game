e0facb8ef16d311fa59168e7afdc589ea77d92c4
shesellsseashells@me.com <shesellsseashells@me.com>
2015-05-23 22:03:53 -0500

    Clear the area just beneath the startout spot (the round shadow of the character), just looks cleaner Remove images not being used (no need to load them)

--- js/engine.js
+++ js/engine.js
@@ -96,6 +96,7 @@ var Engine = (function(global) {
     function update(dt) {
         updateEntities(dt);
         // checkCollisions();
+        ctx.clearRect(200,550,400,130);
     }
 
     /* This is called by the update function and loops through all of the
@@ -188,16 +189,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png', // Add additional images we can use
-        'images/char-cat-girl.png',
-        'images/char-horn-girl.png',
-        'images/char-pink-girl.png',
-        'images/char-princess-girl.png',
-        'images/Heart.png',
-        'images/Key.png',
-        'images/Rock.png',
-        'images/Selector.png',
-        'images/Star.png'
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
